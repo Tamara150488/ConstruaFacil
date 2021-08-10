b9d08c96a6b135eecb0cfb49931536d636e10b5d
ASUS <tamarasilvagomes@mail.com>
2021-08-09 23:13:15 -0300

    teste mobile SauceLabs

---
+++ src/test/java/stepsPO/Runner.java
@@ -0,0 +1,23 @@
+package stepsPO;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.Test;
+
+@Test
+@CucumberOptions(
+        features = {"src/test/resources/featuresPO"},       //onde estão os cenários em Gherkin
+        glue = {"stepsPO"},                                  // Onde estão as definições de passos
+        dryRun = false,                                       //Exibição de log
+        monochrome = true,                                      // Detalhes do log
+        plugin = {
+                    "pretty",                                   //Fz formatação visual do Cucumber
+                    "html:target/reports/extentreports",        // Saída HMTL do Relatório Simples
+                    "json:target/reports/extentreports.json",    //Saída Json com os dados do relatório
+                    //"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/reports/dashboard.html"
+        }
+
+)
+public class Runner extends AbstractTestNGCucumberTests { // Equivale ao @RunWith do JUnit
+    // Configuração de modelo de ralatório ou outa caracteristica
+}
--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -5,23 +5,38 @@ package webTests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
+import utils.Evidencias;
+import utils.Logs;
 
 // 3 - Classe
 public class seleniumSimples {
     // 3.1 - Atributos
 
     WebDriver driver;                           // declarar o objeto do Selenium WebDriver
+    Evidencias evidencias;
+    Logs logs;
+    static String dataHora = new SimpleDateFormat("yyyy-MM-dd HH-mm").format(Calendar.getInstance().getTime());
 
     // 3.2 - Métodos e Funções
+
+    // Executa 1 vez apenas, no início da execução do
+    @BeforeClass
+    public void antesDeTudo() throws IOException {
+        logs = new Logs();
+        logs.iniciarLogCSV(dataHora);
+    }
+
+    // Executa antes de cada @Test
     @BeforeMethod
     public void iniciar(){
         // A - Início
@@ -29,59 +44,92 @@ public class seleniumSimples {
         System.setProperty("webdriver.chrome.driver", "drivers/chrome/91/chromedriver.exe");
         // Instancia o objeto driver como um controlador do Chrome
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().implicitlyWait(120000, TimeUnit.MILLISECONDS);
+
+        evidencias = new Evidencias(); // instanciar
 
     }
 
     @AfterMethod
     public void finalizar(){
-        // C - Encerramento
+        // Parte C - Encerramento
 
-        driver.quit();  // Fecha a janela ao finalizar
-
-    }
-
-    @Test(priority = 2, dependsOnMethods = {"consultarCursoPreparatorioCTFL"})
-    public void consultarCurso(){
-        // B - Realizar o teste
-        driver.get("https://www.iterasys.com.br");                              // Abre o site alvo informado
-
-        driver.findElement(By.id("searchtext")).click();                        // Clica no campo de pesquisa
-        driver.findElement(By.id("searchtext")).clear();                        // Limpa o campo de pesquisa
-        driver.findElement(By.id("searchtext")).sendKeys("mantis");  // Escreve "mantis" no campo
-
-        driver.findElement(By.id("btn_form_search")).click();                   // Clique na lupa
-
-        //assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"mantis\"");
-        assertTrue(driver.findElement(By.cssSelector("h3")).getText().contains("mantis"));
-
-        driver.findElement(By.cssSelector("span.comprar")).click();
-
-        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis");
-        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99");
+        driver.quit(); // Encerra o objeto do Selenium
 
     }
 
     @Test(priority = 1)
-    public void consultarCursoPreparatorioCTFL(){
-
+    public void consultarCursoMantis() throws IOException, InterruptedException {
+        String casoDeTeste = "Consultar Curso Mantis";
         // B - Realizar o teste
+        logs.registrarCSV(casoDeTeste,"Iniciou o teste");
         driver.get("https://www.iterasys.com.br");                              // Abre o site alvo informado
+        logs.registrarCSV(casoDeTeste,"Abriu o site");
+        evidencias.print(driver, dataHora, casoDeTeste, "Passo 1 - Abriu o site");
 
         driver.findElement(By.id("searchtext")).click();                        // Clica no campo de pesquisa
+        logs.registrarCSV(casoDeTeste,"Clicou na caixa de pesquisa");
         driver.findElement(By.id("searchtext")).clear();                        // Limpa o campo de pesquisa
-        driver.findElement(By.id("searchtext")).sendKeys("preparatório ctfl");  // Escreve "mantis" no campo
+        logs.registrarCSV(casoDeTeste,"Limpou a caixa de pesquisa");
+        driver.findElement(By.id("searchtext")).sendKeys("mantis");  // Escreve "mantis" no campo
+        logs.registrarCSV(casoDeTeste,"Escreveu Mantis na caixa de pesquisa");
 
+        Thread.sleep(10000);
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 2 - Digitou Mantis");
         driver.findElement(By.id("btn_form_search")).click();                   // Clique na lupa
+        logs.registrarCSV(casoDeTeste,"Clicou no botão da Lupa");
 
-        //assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"mantis\"");
-        assertTrue(driver.findElement(By.cssSelector("h3")).getText().contains("preparatório ctfl"));
+        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"mantis\"");
+        logs.registrarCSV(casoDeTeste,"Confirmou o texto indicativo da pesquisa do curso Mantis");
 
-        driver.findElement(By.cssSelector("span.comprar")).click();
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 3 - Exibiu a lista de cursos");
+        driver.findElement(By.cssSelector("span.comprar")).click();             // Clica no botão Matricule-se
+        logs.registrarCSV(casoDeTeste,"Clicou no botão Matricule-se");
 
-        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Preparatório CTFL");
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis");
+        logs.registrarCSV(casoDeTeste,"Confirmou o nome do curso Mantis");
+        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99");
+        logs.registrarCSV(casoDeTeste,"Confirmou o preço do curso Mantis");
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 4 - Exibiu o carrinho de compras");
+        logs.registrarCSV(casoDeTeste,"Concluiu o teste");
+    }
+
+    @Test(priority = 2, dependsOnMethods = {"consultarCursoMantis"})
+    public void consultarCursoCTFL() throws IOException {
+        String casoDeTeste = "Consultar Curso CTFL";
+
+        // B - Realizar o teste
+        logs.registrarCSV(casoDeTeste,"Iniciou o teste");
+        driver.get("https://www.iterasys.com.br");                              // Abre o site alvo informado
+        logs.registrarCSV(casoDeTeste,"Abriu o site");
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 1 - Abriu o site");
+
+        driver.findElement(By.id("searchtext")).click();                        // Clica no campo de pesquisa
+        logs.registrarCSV(casoDeTeste,"Clicou na caixa de pesquisa");
+        driver.findElement(By.id("searchtext")).clear();                        // Limpa o campo de pesquisa
+        logs.registrarCSV(casoDeTeste,"Limpou a caixa de pesquisa");
+        driver.findElement(By.id("searchtext")).sendKeys("preparatorio ctfl");  // Escreve "mantis" no campo
+        logs.registrarCSV(casoDeTeste,"Escreveu Preparatório CTFL na caixa de pesquisa");
+
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 2 - Digitou preparatorio ctfl");
+        driver.findElement(By.id("btn_form_search")).click();                   // Clique na lupa
+        logs.registrarCSV(casoDeTeste,"Clicou no botão da Lupa");
+
+        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"preparatorio ctfl\"");
+        logs.registrarCSV(casoDeTeste,"Confirmou o texto indicativo da pesquisa do curso Preparatorio CTFL");
+
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 3 - Exibiu a lista de cursos");
+        driver.findElement(By.cssSelector("span.comprar")).click();             // Clica no botão Matricule-se
+        logs.registrarCSV(casoDeTeste,"Clicou no botão Matricule-se");
+
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText().toUpperCase(), "PREPARATÓRIO CTFL");
+        logs.registrarCSV(casoDeTeste,"Confirmou o nome do curso Preparatorio CTFL");
         assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 169,00");
-
+        logs.registrarCSV(casoDeTeste,"Confirmou o preço do curso Preparatorio CTFL");
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 4 - Exibiu o carrinho de compras");
+        logs.registrarCSV(casoDeTeste,"Concluiu o teste");
     }
 }
+
+
 
